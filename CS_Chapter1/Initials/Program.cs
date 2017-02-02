ccaa589b81a51c2a40637510052b90b1a0ab5d98
washburns1021 <washburns1021@gmail.com>
2017-02-02 17:36:32 -0500

    intiials finished
    I was lit and smoked a joint himey :boom:

--- CS_Chapter1/Initials/Program.cs
+++ CS_Chapter1/Initials/Program.cs
@@ -10,6 +10,15 @@ namespace Initials
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" sss   n    n   w     w");
+            Console.WriteLine("s   s  nn   n   w     w");
+            Console.WriteLine("s      n n  n   w     w");
+
+            Console.WriteLine(" sss   n  n n   w  w  w");
+            Console.WriteLine("    s  n   nn   ww   ww");
+            Console.WriteLine("s   s  n    n   ww   ww");
+            Console.WriteLine(" sss   n    n   w     w");
+
         }
     }
 }
